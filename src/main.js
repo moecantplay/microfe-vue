1aa3c1111e9a730bb3bafaefaea746a03a832139
Gervin Sean Miranda <Tipitaka@Tipitakas-Macbook-Pro.local>
2021-02-22 17:18:04 +0700

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import VueResource from "vue-resource";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.use(VueResource);
+
+Vue.http.headers.common["Access-Control-Allow-Origin"] = "*";
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
